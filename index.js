4e3b83b188ee696d1f605c000974d549bcd82161
Eduardo <edjdevel@gmail.com>
2022-04-08 16:59:26 -0300

    mais um commit

--- index.js
+++ index.js
@@ -10,4 +10,8 @@ app.get('/atualizou', (req, res) => {
     return res.status(200).send({ msg: 'Pow... Funcionou!'})
 });
 
+app.get('/atualizou2', (req, res) => {
+    return res.status(200).send({ msg: 'Funfou denovo!' });
+});
+
 app.listen(3333)
