47d500e63e9d7c22915ea3d044f0847240e0be12
Eduardo <edjdevel@gmail.com>
2022-04-08 17:07:47 -0300

    outro commit

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.status(200).send({ msg: 'Tudo Ok' })
+    return res.status(200).send({ msg: 'Amor! Meu site está no ar! kkkkkk' })
 })
 
 app.get('/atualizou', (req, res) => {
-    return res.status(200).send({ msg: 'Pow... Funcionou!'})
+    return res.status(200).send({ msg: 'Amor! Funcionou!'})
 });
 
 app.get('/atualizou2', (req, res) => {
-    return res.status(200).send({ msg: 'Funfou denovo!' });
+    return res.status(200).send({ msg: 'Gordinha!' });
 });
 
 app.listen(3333)
