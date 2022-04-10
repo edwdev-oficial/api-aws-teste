e71d3eac051bb09decef795ecfe06ca87a5853cb
Eduardo <edjdevel@gmail.com>
2022-04-10 12:15:18 -0300

    alteração das msgs de retorno das rotas

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require ('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.status(200).send({ msg: 'Amor! Meu site está no ar! kkkkkk' })
+    return res.status(200).send({ msg: 'Deploy na AWS - Rota Raiz' })
 })
 
 app.get('/atualizou', (req, res) => {
-    return res.status(200).send({ msg: 'Amor! Funcionou!'})
+    return res.status(200).send({ msg: 'Deploy na AWS - Rota atualizou'})
 });
 
 app.get('/atualizou2', (req, res) => {
-    return res.status(200).send({ msg: 'Gordinha!' });
+    return res.status(200).send({ msg: 'Deploy na AWS - Rota atualizou2' });
 });
 
 app.listen(3333)
